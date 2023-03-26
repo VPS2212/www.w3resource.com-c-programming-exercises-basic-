7b2b65a68b8531699976b560d88ee9e0836699e4
VPS2212 <126179069+VPS2212@users.noreply.github.com>
2023-03-26 12:10:39 +0530

    Add files via upload

---
+++ stohms.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int  sec,h,m,s;
+	printf("Enter the seconds:");
+	scanf("%d",&sec);
+	h=(int)sec/60/60;
+	sec=(int)sec-(h*60*60);
+	m=(int)sec/60;
+	sec=(int)sec-(m*60);
+	s=(int)sec;
+	printf("\n%dH:%dM:%dS\n",h,m,s);
+	return 0;
+}
